c4894f664750f0f069d2ba873a9f3eaf6ace3435
hrithik007 <hrithik.jais2013@gmail.com>
2025-02-01 15:49:04 +0530

    commit

---
+++ src/test/java/com/example/demo/HelloControllerTest.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ExtendWith(SpringExtension.class)
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testIndex() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk()) // Checks if HTTP status is 200
+                .andExpect(content().string("Hii ,This is a demo app.")); // Validates response content
+    }
+}
